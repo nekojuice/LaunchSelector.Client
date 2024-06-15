60160465f77ef628d239fab91db45b1ad805fc59
nekojuice <joseapple7@gmail.com>
2024-06-16 00:23:40 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/LaunchSelector.Client/',
   plugins: [
     vue(),
   ],
   resolve: {
     alias: {
+      //'@': fileURLToPath(new URL('./src', import.meta.url))
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  base: '/LaunchSelector.Client/'
+  }
 })
